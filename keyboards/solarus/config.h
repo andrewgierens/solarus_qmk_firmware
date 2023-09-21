9df327a3c3cf23fe7f893218baf25edbe36a4a26
Andrew Gierens <apgierens@gmail.com>
2023-09-21 21:13:34 +1000

    More things

--- keyboards/solarus/config.h
+++ keyboards/solarus/config.h
@@ -6,18 +6,7 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define ENCODERS_PAD_A { GP27, GP21 }
+#define ENCODERS_PAD_B { GP26, GP22 }
+#define ENCODER_SW F0
+#define ENCODER_RESOLUTION 4
